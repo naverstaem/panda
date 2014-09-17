df0d65afac9b8afdfee0b2df695880ac2011907c
html5coder <romaalupka@yandex.ru>
2014-09-17 20:11:27 +0400

    html blocks, flex slider

--- js/init.js
+++ js/init.js
@@ -1,4 +1,8 @@
 head.load(
 	"js/lib/jquery.js",
+	"js/lib/jquery.fancybox.pack.js",
+	"js/lib/jquery.form.js",
+	"js/lib/jquery.validate.js",
+	"js/lib/jquery.flexslider-min.js",
 	"js/common.js"
 );
---
+++ js/min/init-min.js
@@ -0,0 +1,1 @@
+head.load("js/lib/jquery.js","js/lib/jquery.fancybox.pack.js","js/lib/jquery.form.js","js/lib/jquery.validate.js","js/lib/jquery.flexslider-min.js","js/common.js");
